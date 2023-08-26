d3ad06017ce011e52c75ef93a3508d00519b66ea
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 17:28:28 +0100

    create data for social media links

---
+++ src/components/Footer/data.js
@@ -0,0 +1,22 @@
+export const  socailLinks = [
+    {
+        id : 1,
+        name : 'facebook',
+        url : 'https://www.facebook.com/iam.zakaria.35/'
+    },
+    {
+        id : 2,
+        name : 'instagram',
+        url : 'https://www.instagram.com/zakariae_elhajri/'
+    },
+    {
+        id : 3,
+        name : 'linkedin',
+        url : 'https://www.linkedin.com/in/zakariae-elhajri-526aa719a/'
+    },
+    {
+        id : 4,
+        name : 'github',
+        url : 'https://github.com/ZakariaeELHAJRI'
+    }
+]
