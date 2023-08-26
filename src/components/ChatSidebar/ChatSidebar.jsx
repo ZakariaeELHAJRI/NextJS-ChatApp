f2e71ed041a0f23c6ede3c024bfb19c74e712cf2
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 19:04:58 +0100

    call chat list items component

--- src/components/ChatSidebar/ChatSidebar.jsx
+++ src/components/ChatSidebar/ChatSidebar.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
 import styles from "./ChatSidebar.module.css";
+import ChatListItem from '@/components/ChatListItem/ChatListItem';
+import { conversations } from './data';
+
 export default function ChatSidebar() {
   return (
     <div className={styles.container}>
       <div className={styles["chat-conversation"]}>
-        <p>chat conversations</p>
+        <div className={styles["chat-conversation-body"]}>
+        <div className={`${styles["conversation-list"]} ${styles["customScroll"]}`}>
+            {conversations.map((conversation, index) => (
+              <ChatListItem
+                key={index}
+                name={conversation.name}
+                message={conversation.message}
+                time={conversation.time}
+                profilePhoto={conversation.profilePhoto}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
+
+    
