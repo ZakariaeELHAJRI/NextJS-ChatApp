c3cca42390ef247425f8323ff7eaea32d9d7bbe0
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 19:05:42 +0100

    add conversations and fix scroll style

---
+++ src/components/ChatSidebar/data.js
@@ -0,0 +1,62 @@
+export const conversations = [
+    {
+      name: "John Doe",
+      message: "Hey there!",
+      time: "10:30 AM",
+      profilePhoto: "/images/pic.jpg"
+    },  
+    {
+        name: "Jane Doe",
+        message: "Hey there!",
+        time: "10:30 AM",
+        profilePhoto: "/images/pic.jpg"
+        },
+     {
+            name: "John Doe",
+            message: "Hey there!",
+            time: "10:30 AM",
+            profilePhoto: "/images/pic.jpg"
+    },
+    {
+        name: "John Doe",
+        message: "Hey there!",
+        time: "10:30 AM",
+        profilePhoto: "/images/pic.jpg"
+      },  
+      {
+          name: "Jane Doe",
+          message: "Hey there!",
+          time: "10:30 AM",
+          profilePhoto: "/images/pic.jpg"
+          },
+       {
+              name: "John Doe",
+              message: "Hey there!",
+              time: "10:30 AM",
+              profilePhoto: "/images/pic.jpg"
+      } ,
+      {
+        name: "John Doe",
+        message: "Hey there!",
+        time: "10:30 AM",
+        profilePhoto: "/images/pic.jpg"
+      },  
+      {
+          name: "Jane Doe",
+          message: "Hey there!",
+          time: "10:30 AM",
+          profilePhoto: "/images/pic.jpg"
+          },
+       {
+              name: "John Doe",
+              message: "Hey there!",
+              time: "10:30 AM",
+              profilePhoto: "/images/pic.jpg"
+      }   ,
+        {
+            name: "John Doe",
+            message: "Hey there!",
+            time: "10:30 AM",
+            profilePhoto: "/images/pic.jpg"
+            }
+  ];
