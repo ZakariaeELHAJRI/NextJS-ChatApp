2fda998e726c08921bd9aeec0ed5e547ee264e8f
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-08-26 19:03:09 +0100

    add chat list items

---
+++ src/components/ChatListItem/ChatListItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from "next/image";
+import styles from "./chatListItem.module.css";
+
+const ChatListItem = ({ name, message, time, profilePhoto }) => {
+  return (
+    <div className={styles.container}>
+    <div className={styles["chat-list-item"]}>
+      <div className={styles["user-avatar"]}>
+        <Image src={profilePhoto} alt={`${name}'s avatar`} width={40} height={40} />
+      </div>
+      <div className={styles["chat-details"]}>
+        <div className={styles["user-name"]}>{name}</div>
+        <div className={styles["last-message"]}>{message}</div>
+      </div>
+      <div className={styles["time"]}>{time}</div>
+    </div>
+    </div>
+  );
+}
+
+export default ChatListItem;
