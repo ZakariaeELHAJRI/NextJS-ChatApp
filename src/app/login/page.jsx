5fce345b098e267ed136ba1132391c188c8a9439
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-14 20:47:04 +0100

    create login form

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,5 +1,24 @@
+import styles from './login.module.css';
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className={styles.container}>
+      <div className={styles.login}>
+        <div className={styles.login__logo}>
+          <img src="/images/chat_logo.png" alt="logo" />
+        </div>
+        <div className={styles.login__title}>Login</div>
+        <div className={styles.login__form}>
+          <div className={styles.login__form__input}>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className={styles.login__form__input}>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className={styles.login__form__button}>
+            <button>Login</button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
