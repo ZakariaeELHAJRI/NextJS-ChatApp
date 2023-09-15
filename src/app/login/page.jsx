ff261b1f436d6958f63130acfd6f33993e06d2f5
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-15 21:56:40 +0100

    handle login and save token

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,8 +1,32 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from './login.module.css';
+import axios from 'axios';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        'http://localhost:8000/token',
+        {
+          username,
+          password,
+        }
+      );
+      const accessToken = response.data.access_token;
+      localStorage.setItem('accessToken', accessToken);
+      console.log(accessToken);
+      window.location.href = '/';
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.login}>
@@ -11,18 +35,30 @@ export default function Login() {
         </div>
         <div className={styles.login__title}>Login</div>
         <div className={styles.login__form}>
-          <div className={styles.login__form__input}>
-            <input type="text" placeholder="Username" />
-          </div>
-          <div className={styles.login__form__input}>
-            <input type="password" placeholder="Password" />
-          </div>
-          <div className={styles.login__form__button}>
-            <button>Login</button>
-          </div>
+          <form onSubmit={handleSubmit}>
+            <div className={styles.login__form__input}>
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className={styles.login__form__input}>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className={styles.login__form__button}>
+              <button type="submit">Login</button>
+            </div>
+          </form>
           <p className={styles.registerLink}>
             Don't have an account?
-            <Link href="/register">  Register here</Link>
+            <Link href="/register"> Register here</Link>
           </p>
         </div>
       </div>
