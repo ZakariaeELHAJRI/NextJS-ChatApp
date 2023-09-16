4751a507220dd6c5a544b5843ae21038d128e2ad
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-16 23:41:31 +0100

    add on click props to the component

--- src/components/ChatListItem/ChatListItem.jsx
+++ src/components/ChatListItem/ChatListItem.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Image from "next/image";
 import styles from "./chatListItem.module.css";
 
-const ChatListItem = ({ name, message, time, profilePhoto }) => {
+const ChatListItem = ({ name, message, time, profilePhoto , onClick }) => {
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={onClick}>
     <div className={styles["chat-list-item"]}>
       <div className={styles["user-avatar"]}>
         <Image src={profilePhoto} alt={`${name}'s avatar`} width={40} height={40} />
