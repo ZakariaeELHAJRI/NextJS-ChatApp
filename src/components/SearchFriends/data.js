24c22df1d2817785fb573d9b8e5452a250719367
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-22 22:07:20 +0100

    fetch friends

---
+++ src/components/SearchFriends/data.js
@@ -0,0 +1,20 @@
+// data.js
+
+import axios from 'axios';
+
+
+export const fetchUsers = async () => {
+    try {
+    const token = localStorage.getItem('accessToken');
+    const response = await axios.get('http://localhost:8000/api/friends/', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data; 
+
+  } catch (error) {
+    console.error('Error fetching users:', error);
+    return []; 
+  }
+};
