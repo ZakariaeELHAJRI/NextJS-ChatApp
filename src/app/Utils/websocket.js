e51f4ff5eeb1d639e556628561566e5bfd85818a
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-23 23:22:35 +0100

    create a websoket file to manage initailization of socket and socket functions

---
+++ src/app/Utils/websocket.js
@@ -0,0 +1,86 @@
+// websocket.js
+
+let socket = null;
+
+
+export const initializeSocket = (userId, token) => {
+  if (!socket) {
+    const wsUrl = `ws://localhost:8000/ws/${userId}?token=${token}`;
+    socket = new WebSocket(wsUrl);
+
+    socket.addEventListener('open', (event) => {
+      console.log('WebSocket connection opened:', event);
+    });
+
+    socket.addEventListener('close', (event) => {
+      console.log('WebSocket connection closed:', event);
+    });
+
+    socket.addEventListener('error', (error) => {
+      console.error('WebSocket error:', error);
+    });
+  }
+
+  return socket; 
+};
+
+
+export const sendWebSocketMessage = (messageData) => {
+  if (socket && socket.readyState === WebSocket.OPEN) {
+    const message = {
+      event: 'message',
+      data: messageData,
+    };
+    socket.send(JSON.stringify(messageData));
+    console.log('WebSocket message sent:', messageData);
+  } else {
+    console.error('WebSocket is not open');
+  }
+};
+
+export const sendInvitation = (invitationData) => {
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      const invitationMessage = {
+        event: 'invitation',
+        data: invitationData, // You can define the structure of the invitationData as needed
+      };
+  
+      socket.send(JSON.stringify(invitationMessage));
+      console.log('WebSocket invitation sent:', invitationMessage);
+    } else {
+      console.error('WebSocket is not open');
+    }
+  };
+
+ // Function to handle received messages
+  export const receiveWebSocketMessage = (socket, setMessageState) => {
+    socket.addEventListener('message', (event) => {
+      console.log("WebSocket message received:", event.data);
+  
+      try {
+        // Try to parse the received message as JSON
+        const newMessage = JSON.parse(event.data);
+  
+        // Update the message state with the new message
+        setMessageState((prevMessages) => [...prevMessages, newMessage]);
+      } catch (error) {
+        // Handle non-JSON messages here
+        console.error("Received non-JSON message:", event.data);
+      }
+    });
+  };
+  
+
+  export const receiveWebSocketInvitation = (socket, onReceivedInvitation) => {
+    socket.addEventListener('message', (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        console.log('WebSocket invitation received:', data);
+        onReceivedInvitation(data);
+      } catch (error) {
+        console.error('Received non-JSON message:', event.data);
+      }
+    });
+  };
+
+  
