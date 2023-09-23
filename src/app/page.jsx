3ae7f114215baf1d58b01acb58b755efa84d6b55
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-23 23:27:26 +0100

    using the custom hook useWebSocket

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,85 +1,22 @@
 "use client"
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
 import styles from './page.module.css';
 import Navbar from '@/components/Navbar/Navbar';
 import Footer from '@/components/Footer/Footer';
 import ChatSidebar from '@/components/ChatSidebar/ChatSidebar';
 import ChatConversation from '@/components/ChatConversation/ChatConversation';
-import jwtDecode from 'jwt-decode'; 
-import axios from 'axios'; 
 
-
+import { useWebSocket } from '../context/WebSocketContext'; // Use the custom hook
 
 export default function Home() {
-  const [selectedConversationId, setSelectedConversationId] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
-  const [userData, setUserData] = useState(null);
-  // verify token usestate
-  const [isTokenValid, setIsTokenValid] = useState(false);
-  const router = useRouter();
+  const { isLoading, isTokenValid } = useWebSocket(); // Use the custom hook
 
+  const [selectedConversationId, setSelectedConversationId] = useState(null);
 
   // Callback function to update the selectedConversationId
   const handleConversationSelect = (conversationId) => {
     setSelectedConversationId(conversationId);
   };
-
-// Function to fetch user data by username
-const fetchUserByUsername = async (username) => {
-  try {
-    const token = localStorage.getItem('accessToken');
-    const response = await axios.get(`http://localhost:8000/api/currentuser/${username}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    if (response.status === 200) { 
-      const userData = response.data; 
-      setUserData(userData);
-      console.log(userData);
-      localStorage.setItem('currentUser', userData);
-    
-    } else {
-      console.error('Failed to fetch user data');
-    }
-  } catch (error) {
-    console.error('Error fetching user data:', error);
-  }
-};
-
-  useEffect(() => {
-    // Check if a token is stored (e.g., in localStorage)
-    const token = localStorage.getItem('accessToken');
-    console.log(token);
-    const username = localStorage.getItem('username');
-    fetchUserByUsername(username);
-    if (token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        const currentTime = Math.floor(Date.now() / 1000);
-        if (decodedToken.exp > currentTime) {
-          setIsLoading(false);
-          setIsTokenValid(true);
-
-        } else {
-          setIsLoading(false);
-          router.push('/login');
-        }
-      } catch (error) {
-        setIsLoading(false);
-        router.push('/login');
-      }
-    } else {
-      setIsLoading(false);
-      router.push('/login');
-    }
-
-   
-  }, []);
-
-
 
   if (isLoading) {
     return <div className={styles.loading}>Loading...</div>;
@@ -94,7 +31,7 @@ const fetchUserByUsername = async (username) => {
             <ChatSidebar onItemClick={handleConversationSelect} />
           </div>
           <div className={styles.rightSide}>
-            <ChatConversation conversationId={selectedConversationId}/> 
+            <ChatConversation conversationId={selectedConversationId} />
           </div>
         </main>
         <Footer />
@@ -102,4 +39,5 @@ const fetchUserByUsername = async (username) => {
     );
   }
 }
+
 
