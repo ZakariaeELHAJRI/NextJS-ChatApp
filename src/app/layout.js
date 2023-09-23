4729d1cb97120282fc398c79e47d6b360fb74afa
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-23 23:29:06 +0100

    using the websocketprovider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import {WebSocketProvider} from '../context/WebSocketContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,9 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-       
+       <WebSocketProvider>
         <div className="container">{children}</div>
-        
+       </WebSocketProvider>
         </body>
     </html>
   )
