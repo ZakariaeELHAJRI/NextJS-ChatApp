41ab49ec00796a7f0b09f0b3e6a32665bbf8d91c
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-23 23:24:14 +0100

    using the socket from the context to send invitations

--- src/components/SearchFriends/SearchFriends.jsx
+++ src/components/SearchFriends/SearchFriends.jsx
@@ -1,76 +1,118 @@
 import React, { useEffect, useState } from 'react';
-import styles from "./SearchFriends.module.css";
-import Image from "next/image";
-
-export default function SearchFriends({ UsersList }) {
+import styles from './SearchFriends.module.css';
+import Image from 'next/image';
+import {
+  initializeSocket,
+  sendInvitation,
+  receiveWebSocketInvitation,
+} from '@/app/Utils/websocket';
+import { useWebSocket } from '@/context/WebSocketContext';
+export default function SearchFriends({ UsersList, onReceivedInvitation }) {
   const [friends, setFriends] = useState([]);
+  const {socket} = useWebSocket();
+  const [current_user, setCurrentUser] = useState(null);
+  const userProfilePhoto = '/images/pic.jpg';
 
-  const userProfilePhoto = "/images/pic.jpg";
+  useEffect(() => {
+    const currentUserString = localStorage.getItem('currentUser');
+    const currentUser = JSON.parse(currentUserString);
+    setFriends(UsersList);
+    setCurrentUser(currentUser);
+  }, []);
 
-  useEffect(() => { 
-        setFriends(UsersList);
-        const currentUser =localStorage.getItem('currentUser');
-        console.log(UsersList);
-        console.log(JSON.stringify(currentUser) );
-    }, []);
-  const handelAddFriend = (index) => {
-    // Add the friend to your list
- 
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token && current_user) {
+      const current_user_id = current_user.id;
+
+
+      // Use the receiveWebSocketInvitation function to handle invitations
+    receiveWebSocketInvitation(socket, onReceivedInvitation);
+    }
+  }, [current_user, onReceivedInvitation]);
+
+  const handleAddFriend = (index) => {
+    const friendToAdd = friends[index];
+
+    const data = {
+      event: 'invitation',
+      user_id: current_user.id,
+      friend_id: friendToAdd.id,
+    };
+
+    sendInvitation(data);
+
+    socket.onmessage = (event) => {
+      const acknowledgment = JSON.parse(event.data);
+      if (acknowledgment.event === 'invitation_acknowledgment') {
+        // Handle the acknowledgment as needed
+        if (acknowledgment.status === 'received') {
+          console.log('Invitation received by the user');
+          // Perform further actions, e.g., updating UI
+        } else {
+          console.log('Invitation was not received by the user');
+          // Handle the case where the user did not receive the invitation
+        }
+      }
+    };
   };
 
   const handleRefuse = (index) => {
-    // Remove the friend from your list
-   
+    // Implement this functionality as needed
   };
 
   return (
-    <div className={styles["notification-modal"]}>
-      <div className={`${styles["notification-list"]}  ${styles["customScroll"]}`} onClick={(e) => e.stopPropagation()}>
-     
-      {friends.map((user, index) => (
-        <div key={index} className={styles['invitation']}>
-          <div className={styles['invitation-item']}>
-            <Image
-              src={userProfilePhoto}
-              alt="User"
-              width={40}
-              height={40}
-              className={styles.userImage}
-            />
-            <div className={styles.invitationContent}>
-              <p>
-                <strong>{user.firstname} {user.lastname}</strong>
-              </p>
-              <div className={styles.commonFriends}>
-                <Image
-                  src="/images/pic.jpg"
-                  alt="Common Friends"
-                  width={20}
-                  height={20}
-                  className={styles.commonImage}
-                />
-                <span>{user.common_friends_count} friends in common</span>
+    <div className={styles['notification-modal']}>
+      <div
+        className={`${styles['notification-list']} ${styles['customScroll']}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        {friends.map((user, index) => (
+          <div key={index} className={styles['invitation']}>
+            <div className={styles['invitation-item']}>
+              <Image
+                src={userProfilePhoto}
+                alt="User"
+                width={40}
+                height={40}
+                className={styles.userImage}
+              />
+              <div className={styles.invitationContent}>
+                <p>
+                  <strong>
+                    {user.firstname} {user.lastname}
+                  </strong>
+                </p>
+                <div className={styles.commonFriends}>
+                  <Image
+                    src="/images/pic.jpg"
+                    alt="Common Friends"
+                    width={20}
+                    height={20}
+                    className={styles.commonImage}
+                  />
+                  <span>{user.common_friends_count} friends in common</span>
+                </div>
               </div>
             </div>
+            <div className={styles['invitation-buttons']}>
+              <button
+                className={styles.acceptButton}
+                onClick={() => handleAddFriend(index)}
+              >
+                Add Friend
+              </button>
+              <button
+                className={styles.refuseButton}
+                onClick={() => handleRefuse(index)}
+              >
+                DELETE
+              </button>
+            </div>
           </div>
-          <div className={styles['invitation-buttons']}>
-            <button
-              className={styles.acceptButton}
-              onClick={() => handelAddFriend(index)}
-            >
-              Add Friend
-            </button>
-            <button
-              className={styles.refuseButton}
-              onClick={() => handleRefuse(index)}
-            >
-              DELETE
-            </button>
-          </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
-    </div>
-
   );
-};
+}
