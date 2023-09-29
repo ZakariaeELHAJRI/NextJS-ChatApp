2f5ff97c1772ef46540c531470597ae8f6e14adc
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-09-29 23:41:00 +0100

    add condition to display icon count messages

--- src/components/ChatListItem/ChatListItem.jsx
+++ src/components/ChatListItem/ChatListItem.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import Image from "next/image";
 import styles from "./chatListItem.module.css";
 
-const ChatListItem = ({ name, message, time, profilePhoto , onClick }) => {
+const ChatListItem = ({ name, message, time, profilePhoto, onClick, messageCount,friend_id  }) => {
+  const currentUserString = localStorage.getItem('currentUser');
+  const currentUser = JSON.parse(currentUserString);
+  console.log("friend_id",friend_id + "current_user",currentUser.id)
   return (
     <div className={styles.container} onClick={onClick}>
-    <div className={styles["chat-list-item"]}>
-      <div className={styles["user-avatar"]}>
-        <Image src={profilePhoto} alt={`${name}'s avatar`} width={40} height={40} />
+      <div className={styles["chat-list-item"]}>
+        <div className={styles["user-avatar"]}>
+          <Image src={profilePhoto} alt={`${name}'s avatar`} width={40} height={40} />
+        </div>
+        <div className={styles["chat-details"]}>
+          <div className={styles["user-name"]}>{name}</div>
+          <div className={styles["last-message"]}>{message}</div>
+        </div>
+        <div className="chat-right">
+          <div className={styles["time"]}>{time}</div>
+          {messageCount > 0 && currentUser.id === friend_id && (
+            <div className={styles.messageCount}>{messageCount}</div>
+          )}
+        </div>
       </div>
-      <div className={styles["chat-details"]}>
-        <div className={styles["user-name"]}>{name}</div>
-        <div className={styles["last-message"]}>{message}</div>
-      </div>
-      <div className={styles["time"]}>{time}</div>
-    </div>
     </div>
   );
-}
+};
 
 export default ChatListItem;
+
