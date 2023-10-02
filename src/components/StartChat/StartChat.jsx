e1ab9e9ff3edd09f7feca6bec6fdb7a8a985f56f
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-10-02 22:07:11 +0100

    handle style start page

--- src/components/StartChat/StartChat.jsx
+++ src/components/StartChat/StartChat.jsx
@@ -3,8 +3,11 @@ import styles from "@/components/ChatConversation/ChatConversation.module.css";
 export default function StartChat() {
   return ( 
     <div className={styles["chat-conversation"]}>
-  <h1>Start chatting</h1>
-    <p>Select a conversation to start chatting</p>
+      <div className={styles["center-start-chat"]}>
+          <h1>Start chatting</h1>
+          <p>Select a conversation to start chatting</p>
+      </div>
+
     </div>
   )
 }
