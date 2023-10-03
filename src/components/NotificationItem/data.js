914f0caa80a72257e4e13680efd25da2c735b74e
Zakariae elhajri <zakriaelhajri@gmail.com>
2023-10-03 15:03:53 +0100

    add event name to data sent via socket to create a new conversation

--- src/components/NotificationItem/data.js
+++ src/components/NotificationItem/data.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { sendAcceptance } from "@/app/Utils/websocket";
+import { newConversation } from "@/app/Utils/websocket";
 
 export  const handleSendMsg = async (user_id , friend_id) => {
     try {
@@ -22,13 +22,14 @@ export  const handleSendMsg = async (user_id , friend_id) => {
       if (response.status === 200) {
        console.log("Conversation created successfully");
        const data = {
+        event : "new_conversation" ,
         user1_id: user_id,
         user2_id: friend_id,
       };
-      sendAcceptance(data);
-      console.log("Acceptance sent");
+      newConversation(data);
+      console.log("new conversation sent");
       }
     } catch (error) {
-      console.error("Error sending acceptation:", error);
+      console.error("Error sending new conversation:", error);
     }
   };
